77a8add75f36f2e42571e159f955db30ba7963f5
agg-tech <giesealex6@gmail.com>
2025-02-11 20:44:56 -0500

    Add files via upload

---
+++ Part 2 Exercise 5/party.js
@@ -0,0 +1,27 @@
+const guests = ["ANTONY", "CICERO", "CASSIUS", "CLEOPATRA"];
+
+//Step 1
+guests.unshift('BRUTUS')
+
+//Question 1
+guests.includes('BRUTUS')
+
+//Step 2
+guests.push("AUGUSTUS", "LUCIA")
+
+//Step 3 
+guests.indexOf('SPARTACUS')
+
+//Question 2
+-1
+
+//Step 4
+const updatedList = guests.splice(2, 1)
+
+//Step 5
+const specialInvite = guests.slice(0, 3)
+
+//Step 6
+guests.sort();
+
+guests.unshift('BRUTUS')
